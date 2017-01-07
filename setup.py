f37dd282283896768695103dd067f55d4f9bdadf
Ed Henderson <ed@sharpertool.com>
2017-01-06 18:07:59 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='aws-pricing-lib',
+    packages=['aws-pricing-api'],
+    version='0.1',
+    description='Amazon Web Servcies pricing API Library functions',
+    author='Ed Henderson',
+    author_email='ed@sharpertool.com',
+    url='https://github.com/sharpertool/aws-pricing-lib',
+    download_url='https://github.com/sharpertool/aws-pricing-lib/tarball/0.1',
+    keywords=['aws', 'api'],
+    classifiers=[],
+)
